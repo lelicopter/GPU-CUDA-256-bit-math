595bb6c820ed60f3cf3fd137123741e4caa88aee
Siranu Kak Berzherak <lelicopter@mail.ru>
2025-08-22 15:41:02 +1000

    Update compile.cmd

--- compile.cmd
+++ compile.cmd
@@ -4,7 +4,7 @@ del u256_benchmark.exe
 
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 
-nvcc -rdc=true -use_fast_math --expt-relaxed-constexpr -Xcompiler "/wd 4819" -Xptxas=-v,-O3 -O3 -arch=sm_75 --extended-lambda -std=c++20 -o u256_benchmark.exe u256_benchmark.cu
+nvcc -rdc=true -use_fast_math --expt-relaxed-constexpr -Xcompiler "/wd 4819" -Xptxas=-v,-O3 -O3 -arch=native --extended-lambda -std=c++20 -o u256_benchmark.exe u256_benchmark.cu
 
 del u256_benchmark.exp
 del u256_benchmark.lib
